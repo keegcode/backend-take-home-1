3f8873be1aa7c217caa46eef2cdb50b3121607a2
Danylo Matseruk <ukrobzoru@gmail.com>
2022-06-21 17:24:38 +0300

    Added knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+
+export default {
+    client: 'postgres',
+    connection: {
+        host : process.env.POSTGRES_HOST || '127.0.0.1',
+        port : process.env.POSTGRES_PORT || 5432,
+        user : process.env.POSTGRES_USER,
+        password : process.env.POSTGRES_PASSWORD,
+        database : process.env.POSTGRES_DB
+    },
+    acquireConnectionTimeout: 1000,
+    migrations: {
+        directory: 'database/migrations'
+    },
+}
